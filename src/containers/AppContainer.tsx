f9fd89eab1d8c64328e64af9ed3e7921f2bf94ab
Nosherwan Ghazanfar <nosh@converlens.com>
2018-09-15 07:48:56 +1000

    main applicaiton container

---
+++ src/containers/AppContainer.tsx
@@ -0,0 +1,35 @@
+import React, { PureComponent } from 'react';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+import { history } from '../navigation/history';
+import { configureStore } from '../store/configureStore';
+import Routes from '../navigation/routes';
+import { Navbar } from '../components/Navbar';
+
+
+class AppContainer extends PureComponent {
+
+	store: any
+
+	componentWillMount() {
+		this.store = configureStore();
+	}
+
+	render() {
+		const {
+			store
+		} = this;
+		return (
+			<Provider store={store}>
+				<ConnectedRouter history={history}>
+					<>
+						<Navbar key={0} />
+						<Routes key={1} />
+					</>
+				</ConnectedRouter>
+			</Provider>
+		);
+	}
+}
+
+export default AppContainer;
