c3b61accd6840e61966b61a22c311b9fba52446f
Nosherwan Ghazanfar <nosh@converlens.com>
2018-09-15 07:47:38 +1000

    simple navigation for logical pages

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { push } from 'react-router-redux';
+import { connect } from 'react-redux';
+
+const styles = require('./styles.css');
+
+const buttons = [
+  { name: 'main', path: '/' },
+  { name: 'profile', path: '/profile' }
+];
+
+interface INavbar {
+  dispatch: any
+}
+
+class NavbarComponent extends Component<INavbar, any> {
+
+  constructor(props: any) {
+    super(props)
+  }
+
+  render() {
+    const { dispatch } = this.props
+
+    return (
+      <div className={styles.nav_container}>
+        {buttons.map((btn: any) =>
+          <button
+            key={btn.name}
+            onClick={((e: any) => {
+              dispatch(push(btn.path))
+            }
+            )}
+          >
+            {btn.name}
+          </button>)}
+      </div>
+    );
+  }
+}
+
+const Navbar = connect()(NavbarComponent)
+
+export { Navbar }
