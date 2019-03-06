f4f4febc775ae469a7dfafad5c707565aa97f672
Nosh Ghazanfar <nosherwan@gmail.com>
2019-03-06 23:07:22 +1100

    added one more test, updated snapshot

--- src/components/FormField/index.test.tsx
+++ src/components/FormField/index.test.tsx
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme';
 import { FormField } from './index';
 
 describe('FormField', () => {
-  it('should render correctly in "debug" mode', () => {
+  it('should render correctly', () => {
     const props = {
       label:'First Name',
       innerRef: (ref: any) => ref,
@@ -17,5 +17,22 @@ describe('FormField', () => {
 
     const component = shallow(<FormField {...props} />);
     expect(component).toMatchSnapshot();
+  });
+
+  it('should have input & label', () => {
+    const props = {
+      label:'First Name',
+      innerRef: (ref: any) => ref,
+      name: 'firstName',
+      value: '',
+      error: '',
+      changeHandler: () => {
+        return;
+      }
+    };
+
+    const component = shallow<FormField>(<FormField {...props} />);
+    expect(component.find('input')).toHaveLength(1);
+    expect(component.find('label').text()).toEqual('First Name');
   });
 });
